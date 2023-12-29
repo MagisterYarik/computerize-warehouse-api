0cee51c7983a32847e3fc0ec422f02072c6138c0
Dvirnik Yaroslav <ultve@yandex.ru>
2023-12-29 22:23:23 +0300

    refactor request_source from char to short for real

--- src/main/java/telran/coumputerizedWarehouse/dto/OrderRequestClose.java
+++ src/main/java/telran/coumputerizedWarehouse/dto/OrderRequestClose.java
@@ -1,5 +1,5 @@
 package telran.coumputerizedWarehouse.dto;
 
-public record OrderRequestClose(long order_id, char request_source ) {
+public record OrderRequestClose(long order_id, short request_source ) {
 
 }
