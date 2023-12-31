d1329490bb8ace06dc295c5a25adcd8c59740b86
Dvirnik Yaroslav <ultve@yandex.ru>
2023-12-31 12:51:35 +0300

    Add setters for entities

--- src/main/java/telran/coumputerizedWarehouse/entity/OrderHeader.java
+++ src/main/java/telran/coumputerizedWarehouse/entity/OrderHeader.java
@@ -10,10 +10,12 @@ import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @NoArgsConstructor
 @Getter
+@Setter
 @EqualsAndHashCode
 @ToString
 @Entity
